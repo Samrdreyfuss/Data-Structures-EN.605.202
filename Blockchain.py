dba2dc1a3245f2c8ed32a0f17a762bc85ef163b8
Sam Dreyfuss <samrdreyfuss@gmail.com>
2022-08-15 10:56:18 -0700

    1 change

---
+++ Blockchain.py
@@ -0,0 +1,11 @@
+'''
+Creating a blockchain from scratch
+'''
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
