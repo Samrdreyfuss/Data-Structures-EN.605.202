d2f188f90687f3cbbf880dfe7863839bb579da5a
Sam Dreyfuss <samrdreyfuss@gmail.com>
2022-08-17 09:45:45 -0700

    1 change

--- Create_Files.py
+++ Create_Files.py
@@ -36,3 +36,34 @@ def Create_Multiple_Files():
         Create_File('Descending_' + str(size) + '.txt', size, False, False)
         Create_File('Random_' + str(size) + '.txt', size, True, True)
 
+def Confirm_No_Duplicates_In_File(filename: str):
+    with open(filename, 'r') as file:
+        array = file.readlines()
+        for i in range(len(array)):
+            for j in range(i + 1, len(array)):
+                if array[i] == array[j]:
+                    return True
+    return False
+
+def Create_Files_With_Duplicates():
+    for size in [10000, 5000, 2000, 1000, 50]:
+        with open('Random_Test_' + str(size) + '.txt', 'w') as file:
+            for i in range(size):
+                file.write(str(randint(0, size)) + '\n')
+
+    for size in [10000, 5000, 2000, 1000, 50]:
+        if Confirm_No_Duplicates_In_File('Random_Test_' + str(size) + '.txt'):
+            print('Random_Test_' + str(size) + '.txt : Has Duplicates In It')
+        else:
+            print('Random_Test_' + str(size) + '.txt : No Duplicates')
+
+    for size in [10000, 5000, 2000, 1000, 50]:
+        if Confirm_No_Duplicates_In_File('Random_Test_' + str(size) + '.txt'):
+            print('Random_' + str(size) + '.txt : Has Duplicates In It')
+        else:
+            print('Random_' + str(size) + '.txt : No Duplicates')
+
+
+
+
+
