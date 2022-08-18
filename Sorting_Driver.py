bb089ce097709e71876d21a7666c3bfef49becbd
Sam Dreyfuss <samrdreyfuss@gmail.com>
2022-08-17 20:41:26 -0700

    1 change

--- Sorting_Driver.py
+++ Sorting_Driver.py
@@ -9,18 +9,39 @@ Sources:
 '''
 
 from Quicksort import Iterative_Quick_Sort_Version_1
+import os
 
 #Driver code
 if __name__ == '__main__':
     print('Code Running As Expected')
 
+    #search for .txt files
+    ext = ('.txt')
+    all_files = []
+    for files in os.listdir():
+        if files.endswith(ext):
+            all_files.append(files)
+        else:
+            continue
+    #all_files = all_files.sort(reverse=True)
+    for file in all_files:
+        with open(file, 'r') as file_in:
+            for line in file_in:
+                # extract new line from given file
+                line = line.strip()
 
+                # if there are any blank lines within .txt file, ignore them
+                if line == '':
+                    continue
+                array = line
+
+                print(array)
     # Driver code
-    array = [9,0,8,1,7,3,6,4,10,20,5,14,6,8,9,10,5,1,2]
-    Iterative_Quick_Sort_Version_1(array, 0, len(array) - 1)
+    #array = [9,0,8,1,7,3,6,4,10,20,5,14,6,8,9,10,5,1,2]
+                Iterative_Quick_Sort_Version_1(array, 0, len(array) - 1)
     #swap_count_final_value =
 
-    print(f'Sorted array: {array}')
+    #print(f'Sorted array: {array}')
 
 
 
